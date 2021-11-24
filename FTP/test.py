520ad79372593a03fa8649258fdea9693566a51e
asingh48 <asingh48@ncsu.edu>
2021-11-24 12:01:42 -0500

    New Updates

--- FTP/test.py
+++ FTP/test.py
@@ -7,6 +7,7 @@ import os
 import sys
 import time
 import random
+import struct
 
 # SERVER_HOST = "127.0.0.1"
 SERVER_HOST = socket.gethostname()
@@ -24,7 +25,8 @@ class Client:
         self.BUFFER = []
         self.BUFFER_LENGTH = 64
         self.WINDOW = []
-        self.ACKS = []
+        self.TRACK_PACKET = pd.DataFrame()
+        self.SEQUENCE_NUMBER = 0
 
     def __str__(self):
         return f'{self.HOSTNAME},{self.PORT},{self.SERVER_HOST},{self.SERVER_PORT},{self.FILE_INPUT},{self.N},{self.MSS}'
@@ -40,14 +42,12 @@ class Client:
                 piece = in_file.read(1)
                 yield piece.decode('utf-8')     # RETURN 1Byte Read
                 if piece == b'':
-                    break               # end of file
+                    break                       # end of file
 
     def maintain_buffer(self):
         flag = 0
         while True:
             if len(self.BUFFER) < self.BUFFER_LENGTH and flag == 0:
-                # data = self.rdt_send()
-                # print(len([data for data in self.rdt_send()]))
                 try:
                     self.BUFFER.append(next(self.rdts))
                 except StopIteration:
@@ -56,17 +56,61 @@ class Client:
                     break
                 # print(f"ADDING BYTE TO BUFFER - {self.BUFFER}")
                 
+    # def checksum(self, raw_data):
+    #     checksum = 0
+    #     data_len = len(raw_data)
+    #     # print(type(raw_data))
+    #     if (data_len % 2):
+    #         data_len += 1
+    #         # print(type(struct.pack('!B', 0)))
+    #         raw_data += struct.pack('!B', 0)
+        
+    #     for i in range(0, data_len, 2):
+    #         w = (raw_data[i] << 8) + (raw_data[i + 1])
+    #         checksum += w
+
+    #     checksum = (checksum >> 16) + (checksum & 0xFFFF)
+    #     checksum = ~checksum & 0xFFFF
+    #     return f'{checksum:016b}'
+
+    def carry_around_add(self, x, y):
+        return ((x+y) & 0xffff) + ((x + y) >> 16)
+
+    def checksum_computation(self, message):
+        add = 0
+        for i in range(0, len(message) - len(message) % 2, 2):
+            message = str(message)
+            w = ord(message[i]) + (ord(message[i + 1]) << 8)
+            add = self.carry_around_add(add, w)
+        return ~add & 0xffff
+    
+    def pack_packet(self, packet):
+        data = ""
+        data += str(f'{self.SEQUENCE_NUMBER}') + "|"        # {self.SEQUENCE_NUMBER:032b}
+        self.SEQUENCE_NUMBER += 1
+        data += str(f'0101010101010101') + "|"
+        data += str(packet)
+
+        ch = self.checksum_computation(data)
+
+        data = ""
+        data += str(f'{self.SEQUENCE_NUMBER:032b}') + "|"
+        data += str(f'{ch:016b}') + "|"
+        data += str(f'0101010101010101') + "|"
+        data += str(packet)
+
+        print(data)
+        return data
 
     def create_window(self):
         while True:
             if len(self.WINDOW) < self.N and len(self.BUFFER) >= self.MSS:
-                
                 packet = self.BUFFER[:self.MSS]
                 self.BUFFER = self.BUFFER[self.MSS:]
                 packet = "".join(packet)
+                packet = self.pack_packet(packet)
                 self.WINDOW.append(packet)
-                self.ACKS.append(True)
-                # print(f"CREATING WINDOW PACKET - {self.WINDOW}")
+                print(f"CREATING WINDOW PACKET - {self.WINDOW}")
 
     def checkTime():
         pass
@@ -84,24 +128,12 @@ class Client:
             if len(self.WINDOW) > 0:
                 # print(f"IN SEND")
                 try:
-                    print(f"SENDING PACKET FROM WINDOW - {self.WINDOW}")
+                    # print(f"SENDING PACKET FROM WINDOW - {self.WINDOW}")
                     # print(self.WINDOW[0], end="")
                     packet_to_send = self.WINDOW.pop(0)
-                    print(f'PACKET SENT = {packet_to_send}')
+                    # print(f'PACKET SENT = {packet_to_send}')
                     thread_acks = threading.Thread(target=self.socket_send,args=(packet_to_send,))
                     thread_acks.start()
-
-
-
-
-
-
-
-
-
-
-
-
 
                     # print(f"SENT PACKET FROM WINDOW - {self.WINDOW}")
                     # self.BUFFER = self.BUFFER[self.MSS:]
@@ -113,7 +145,6 @@ class Client:
 
 
     def gobackn_client(self):
-        # self.maintain_buffer()
         self.rdts = self.rdt_send()
         thread_maintain_buffer = threading.Thread(target=self.maintain_buffer)
         thread_maintain_buffer.start()
@@ -124,20 +155,6 @@ class Client:
         thread_send_packet = threading.Thread(target=self.send_packet)
         thread_send_packet.start()
 
-        # buffer = []
-        # window = []
-        # while True:
-        #     while len(buffer) < 1024:
-        #         buffer.append(self.rdt_send())
-
-        #     while len(window) < self.N:
-
-
-
-
-        #     for packet in self.rdt_send():
-        #         print((self.SERVER_HOST, self.SERVER_PORT))
-        #         self.sock.sendto(packet, (self.SERVER_HOST, self.SERVER_PORT))
 
             
 
