875d86189fd1acda51e71b12975dd14f4ab725cb
Nitish <nitishtalekar.nt503@gmail.com>
2021-11-26 18:40:10 -0500

    Revert "Merge branch 'main' of https://github.com/nitishtalekar/CSC-573-InternetProtocols into main"
    This reverts commit cfa1e329f622eca83ef53b60d195be405b3c10d5, reversing
    changes made to b337f7e277486801ea10f6d69487a36f2a0bffed.

--- FTP/Reciever.py
+++ FTP/Reciever.py
@@ -14,42 +14,13 @@ import sys
 # S1 = Server(PORT, FILE_OUTPUT, PROB_LOSS_SERVICE)
 # S1.rdt_rcv()
 
-# TASK 1
-print("\n\n-------------TASK 1-------------\n\n")
-PORT = 7735
-FILE_OUTPUT = 'output.txt'
-PROB_LOSS_SERVICE = 0.05
-for i in range(11):
-    for j in range(5):
-        print(f"RECIEVING FILE: ITERATION {j+1}")
-        S1 = Server(PORT, FILE_OUTPUT, PROB_LOSS_SERVICE)
-        S1.rdt_rcv()
-        S1.sock.close()
-        print("\n\n\n")
 
-# TASK 2
-print("\n\n-------------TASK 2-------------\n\n")
 PORT = 7735
 FILE_OUTPUT = 'output.txt'
 PROB_LOSS_SERVICE = 0.05
 for i in range(10):
     for j in range(5):
-        print(f"RECIEVING FILE: ITERATION {j+1}")
-        S1 = Server(PORT, FILE_OUTPUT, PROB_LOSS_SERVICE)
-        S1.rdt_rcv()
-        S1.sock.close()
-        print("\n\n\n")
-
-
-# TASK 3
-print("\n\n-------------TASK 3-------------\n\n")
-PORT = 7735
-FILE_OUTPUT = 'output.txt'
-PROB_LOSS_SERVICE_LIST = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]
-# PROB_LOSS_SERVICE = 0.05
-for PROB_LOSS_SERVICE in PROB_LOSS_SERVICE_LIST:
-    for j in range(5):
-        print(f"RECIEVING FILE: PROBLOSS {PROB_LOSS_SERVICE} {j+1}")
+        print(f"RECIEVING FILE: ITERATION {i+1}")
         S1 = Server(PORT, FILE_OUTPUT, PROB_LOSS_SERVICE)
         S1.rdt_rcv()
         S1.sock.close()
