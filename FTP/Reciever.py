b4763b16d34ff8c7bc1d9cb2c15cbc0ca68798c9
asingh48 <aayushipsingh@gmail.com>
2021-11-26 17:00:03 -0500

    FTP-WITH 3 TASKS

--- FTP/Reciever.py
+++ FTP/Reciever.py
@@ -14,13 +14,39 @@ import sys
 # S1 = Server(PORT, FILE_OUTPUT, PROB_LOSS_SERVICE)
 # S1.rdt_rcv()
 
+# TASK 1
+PORT = 7735
+FILE_OUTPUT = 'output.txt'
+PROB_LOSS_SERVICE = 0.05
+for i in range(11):
+    for j in range(5):
+        print(f"RECIEVING FILE: ITERATION {j+1}")
+        S1 = Server(PORT, FILE_OUTPUT, PROB_LOSS_SERVICE)
+        S1.rdt_rcv()
+        S1.sock.close()
+        print("\n\n\n")
 
+# TASK 2
 PORT = 7735
 FILE_OUTPUT = 'output.txt'
 PROB_LOSS_SERVICE = 0.05
 for i in range(10):
     for j in range(5):
-        print(f"RECIEVING FILE: ITERATION {i+1}")
+        print(f"RECIEVING FILE: ITERATION {j+1}")
+        S1 = Server(PORT, FILE_OUTPUT, PROB_LOSS_SERVICE)
+        S1.rdt_rcv()
+        S1.sock.close()
+        print("\n\n\n")
+
+
+# TASK 3
+PORT = 7735
+FILE_OUTPUT = 'output.txt'
+PROB_LOSS_SERVICE_LIST = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]
+# PROB_LOSS_SERVICE = 0.05
+for PROB_LOSS_SERVICE in PROB_LOSS_SERVICE_LIST:
+    for j in range(5):
+        print(f"RECIEVING FILE: PROBLOSS {PROB_LOSS_SERVICE} {j+1}")
         S1 = Server(PORT, FILE_OUTPUT, PROB_LOSS_SERVICE)
         S1.rdt_rcv()
         S1.sock.close()
